49f2577090366e84aafb6c953cc8485c771c0d40
Badal Chauhan <badalch10@gmail.com>
2025-03-19 23:56:03 +0530

    created search

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
+import React from "react";
+import Search from "./components/Search";
+
 function App() {
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </>
+    <main>
+      <div className="pattern" />
+
+      <div className="wrapper" />
+        <header>
+          <img src="./hero-img.png" alt="Hero Banner" />
+          <h1 className="mx-auto max-w-4xl text-center text-5xl font-bold leading-tight tracking-[-1%] text-white sm:text-[64px] sm:leading-[76px]">Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
+        </header>
+
+        <Search />
+    </main>
   )
 }
 
