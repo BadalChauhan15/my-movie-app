d49fef3f067d680acba69017fa665c781fc5b1ab
Badal Chauhan <badalch10@gmail.com>
2025-03-19 22:24:52 +0530

    changes in components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
-import Card from "./components/Card";
-
 function App() {
 
   return (
     <>
-      <h1>Movie App</h1>
-      <div className='card__container'>
-        <Card title="Captain Amreica: Winter Soldier" />
-        <Card title="Captain Amreica: Civil War" />
-        <Card title="Captain Amreica: Brave New World" />
-      </div>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </>
   )
 }
